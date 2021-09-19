30c3e86ad1a686fc1c9dbd0493794b8ab25478a7
Priscila05 <priscilacosgomNotebook@outlook.com>
2021-09-19 17:10:23 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class main
+    public static void main ( String[] args) throws {
+    public static final String[] Piloto = null;
+    public static final String[] Pitolo = null;
+
+        System.out.println("Digite nome do piloto: ");
+        System.out.println("Digite o CPF do piloto: ");
+        System.out.println("Digite o tamanho do armazenanto: ");
+
+        String [] pilotoStrings = new String [];
+  
+        for(int i = 0; i < piloto; i++){
+
+            piloto2[i] = piloto[i];
+        }
+
+        for(int i = 0; i < piloto; i++){
+            
+            System.out.println(Piloto[i]);
+        }
     }
-}
